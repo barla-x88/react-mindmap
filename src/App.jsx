4bd2a2f7ac263611496059a8d4b74a60a41a9124
Barla Ankit <barla.x88@gmail.com>
2024-03-22 17:01:32 +0530

    Setup Background

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import ReactFlow, {
+  Background,
+  BackgroundVariant,
+  Controls,
+  MiniMap,
+} from 'reactflow';
+import 'reactflow/dist/style.css';
 import './App.css';
-
 function App() {
-  return <h1>React MindMap</h1>;
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow>
+        <Background color="#EFECEC" style={{ backgroundColor: '#191A1B' }} />
+        <Controls />
+        <MiniMap />
+      </ReactFlow>
+    </div>
+  );
 }
 
 export default App;
